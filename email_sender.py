3c1c4dcdd8c204ac5f5cec2231cfe4b5f7e9dfe0
Suresh Krishna G <suresh2004krishna@gmail.com>
2026-06-11 16:09:46 +0530

    Refactor email sending to use Resend API

--- email_sender.py
+++ email_sender.py
@@ -1,29 +1,15 @@
 from email.mime.text import MIMEText
-from loder import get_env
 import smtplib
+import resend
+from loder import get_env
 
-EMAIL = get_env("MAIL_USERNAME")
-APP_PASSWORD = get_env("MAIL_PASSWORD")
+resend.api_key = get_env("RESEND_API_KEY")
 
 def send_welcome_email(student_name, student_email):
 
-    subject = "Welcome to Synergy Python Course"
-
-    body = f"""
-Hello {student_name},
-
-Welcome to Synergy!
-...
-"""
-
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = EMAIL
-    msg['To'] = student_email
-
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(EMAIL, APP_PASSWORD)
-
-    server.send_message(msg)
-    server.quit()
+    resend.Emails.send({
+        "from": "onboarding@resend.dev",
+        "to": student_email,
+        "subject": "Welcome to Synergy Python Course",
+        "text": f"Hello {student_name}, Welcome to Synergy!"
+    })
