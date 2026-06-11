7c8d12406eb5c09b701ca239714f7875f7c118d7
Suresh Krishna G <suresh2004krishna@gmail.com>
2026-06-11 16:26:25 +0530

    Update email sending logic in email_sender.py
    Refactor email sending to use new resend API structure with HTML content.

--- email_sender.py
+++ email_sender.py
@@ -1,52 +1,28 @@
-from resend import Resend
+import resend
 from loder import get_env
 
-client = Resend(
-    api_key=get_env("RESEND_API_KEY")
-)
+resend.api_key = get_env("RESEND_API_KEY")
 
 def send_welcome_email(student_name, student_email):
 
-    client.emails.send({
+    params = {
         "from": "onboarding@resend.dev",
         "to": [student_email],
         "subject": "Welcome to Synergy Python Course",
-        "text": f"""
-    Hello {student_name},
-    
-    Welcome to Synergy!
-    
-    Your registration has been successfully completed.
-    
-    What you will receive:
-    
-    ✅ Live Python Classes
-    ✅ Daily Assignments
-    ✅ Hands-on Projects
-    ✅ Certificate of Completion
-    
-    We will contact you soon with:
-    
-    - Course Schedule
-    - Google Meet Link
-    - WhatsApp Community Link
-    
-    Thank you for choosing Synergy.
-    
-    Regards,
-    Synergy Team
-    """
-        })
+        "html": f"""
+        <h2>Welcome {student_name}!</h2>
 
-try:
+        <p>Your registration has been completed successfully.</p>
 
-    send_welcome_email(
-        name,
-        email
-    )
+        <ul>
+            <li>Live Python Classes</li>
+            <li>Daily Assignments</li>
+            <li>Hands-on Projects</li>
+            <li>Certificate of Completion</li>
+        </ul>
 
-    print("Welcome email sent")
+        <p>Thank you for choosing Synergy.</p>
+        """
+    }
 
-except Exception as e:
-
-    print("Email Error:", e)
+    resend.Emails.send(params)
